211558a88f12f75c726c088cce89ea82cb73d2fa
RafaelNovais <54107877+RafaelNovais@users.noreply.github.com>
2020-07-15 18:22:17 -0300

    Up2
    Up2

---
+++ Main/qaMain.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using TestBooking.qaGeneral;
+
+namespace TestBooking.Main
+{
+    [TestFixture]
+    class qaMain : qaSetup
+    {
+
+        [Test]
+        public void T1_DriverExecute()
+        {
+            String Local = "Limerick, Limerick County, Irlanda";
+
+            driver.Navigate().GoToUrl("https://www.booking.com/");
+
+            IWebElement webElement = null;
+            webElement = driver.FindElement(By.Id("ss"));
+            webElement.SendKeys(Local);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            IWebElement webElementSelect = null;
+            webElementSelect = driver.FindElement(By.CssSelector(".sb-autocomplete__item-with_photo:nth-child(1)"));
+            webElementSelect.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            IWebElement webElementSearch = null;
+            webElementSearch = driver.FindElement(By.CssSelector(".sb-searchbox__button > span:nth-child(1)"));
+            webElementSearch.Click();
+        }
+
+
+    }
+}
---
+++ qaGeneral/qaSetup.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestBooking.qaGeneral
+{
+    public abstract class qaSetup
+    {
+
+        public static IWebDriver driver;
+        public IDictionary<string, object> vars { get; private set; }
+        private IJavaScriptExecutor js;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver("D:/Arquivos/Desenvolvimento/Teste Rafael/TestKneat/Driver");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            js = (IJavaScriptExecutor)driver;
+            vars = new Dictionary<string, object>();
+        }
+    }
+}
